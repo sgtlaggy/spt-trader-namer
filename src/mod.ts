0b99518dd5e2e8b1bb4f67e36147bced0c6d96ef
sgtlaggy <8661717+sgtlaggy@users.noreply.github.com>
2025-04-01 13:48:13 -0700

    support changing avatar/details via trader ID or avatar ID

--- src/mod.ts
+++ src/mod.ts
@@ -18,25 +18,32 @@ class Mod implements IPostDBLoadMod {
         const locales = db.getLocales();
         const en = locales.global.en;
 
-        const traders: Record<string, ITraderBase> = Object.fromEntries(
-            Object.values(db.getTraders())
-                .map((trader) => [en[`${trader.base._id} Nickname`], trader.base])
-                .filter((([nick, _]) => (nick !== undefined))));
+        // up to 3 entries per trader: ID, Nickname, Avatar ID
+        const traders: Record<string, ITraderBase> = {};
+        Object.values(db.getTraders()).forEach((trader) => {
+            const id = trader.base._id;
+            const nick = en[`${id} Nickname`];
+            // older traders like Prapor/Therapist have mismatched ID and avatar ID
+            // this is mostly for compatibility with other avatar switcher mods
+            const avatar = trader.base.avatar.split('.')[0].split('/').at(-1);
+            traders[id] = traders[nick] = traders[avatar] = trader.base;
+        });
 
-        for (const [oldName, newName] of Object.entries(NAMES)) {
-            const trader = traders[oldName];
+        for (const [nameOrId, newName] of Object.entries(NAMES)) {
+            const trader = traders[nameOrId];
             var details = newName;
             if ((typeof newName) === "string") {
                 details = { Nickname: newName as string }
             }
 
             if (!trader) {
-                logger.info(`[TraderNamer] ${oldName} does not exist, not changing name.`);
+                logger.info(`[TraderNamer] ${nameOrId} does not exist, not changing name.`);
                 continue;
             }
 
+            const nick = en[`${trader._id} Nickname`] || trader.nickname || nameOrId;
             for (const [key, value] of Object.entries(details)) {
-                logger.info(`[TraderNamer] Changing ${oldName}'s ${key} to ${value}`);
+                logger.info(`[TraderNamer] Changing ${nick}'s ${key} to ${value}`);
             }
 
             for (const locale of Object.values(locales.global)) {
@@ -52,15 +59,15 @@ class Mod implements IPostDBLoadMod {
 
         for (const image of fileSystem.getFiles(imagesPath)) {
             // getFiles gives names with leading ‘/’
-            const traderName = image.split(".")[0].substring(1);
-            const trader = traders[traderName];
+            const nameOrId = image.split(".")[0].substring(1);
+            const trader = traders[nameOrId];
 
             if (!trader) {
-                logger.info(`[TraderNamer] ${traderName} does not exist, not changing avatar.`);
+                logger.info(`[TraderNamer] ${nameOrId} does not exist, not changing avatar.`);
                 continue;
             }
 
-            logger.info(`[TraderNamer] Changing ${traderName}'s avatar.`);
+            logger.info(`[TraderNamer] Changing ${nameOrId}'s avatar.`);
 
             const avatarRouteNoExt = trader.avatar.split(".")[0];
             const imagePath = path.join(imagesPath, image);
